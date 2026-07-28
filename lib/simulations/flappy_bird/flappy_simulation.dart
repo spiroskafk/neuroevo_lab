9b7d93410d5c1e0e77dbb2c8259f13f993184c91
Spiros Kafkias <spiros.kafkias@dialecticanet.com>
2026-07-29 02:36:15 +0300

    Tune Flappy NEAT config: higher mutation rates, lower threshold

--- lib/simulations/flappy_bird/flappy_simulation.dart
+++ lib/simulations/flappy_bird/flappy_simulation.dart
@@ -20,11 +20,14 @@ class FlappyBirdSimulation extends SimulationBase {
       : super(
           config: const SimulationConfig(),
           neatConfig: NEATConfig(
-          numInputs: 4,
-          numOutputs: 1,
+            numInputs: 4,
+            numOutputs: 1,
             populationSize: 100,
             maxStagnation: 25,
-            compatibilityThreshold: 1.5,
+            compatibilityThreshold: 1.0,
+            mutateAddNodeRate: 0.1,
+            mutateAddConnectionRate: 0.15,
+            mutateWeightsPower: 0.8,
           ),
         );
 
